19810c0030d34fdf902910c7635cc45f718008a0
CAEUser <6@dy[hYD>
2018-12-18 16:35:15 +0100

    Generated microservice version 0.1

---
+++ db/hossner-scheduler_create_tables.sql
@@ -0,0 +1,18 @@
+--
+-- Database Schema:  
+-- Automatically generated sql script for the service hossner-scheduler, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table event.
+--
+CREATE TABLE .event (
+  date date ,
+  description text ,
+  id integer ,
+CONSTRAINT id_PK PRIMARY KEY (id)
+ 
+);
+
+
+
